fee2890fe187b7509f558ab39555088a65491e74
James Lewis <dptoot@gmail.com>
2016-12-04 13:30:54 -0500

    turn off unsupported notification permission request

--- app/web/notifications.js
+++ app/web/notifications.js
@@ -1,11 +1,11 @@
 const src = require('./images/doug-icon.png');
 
 export function requestNotificationPermission() {
-	if (window.Notification) {
-		window.Notification.requestPermission().then(function(result) {
-		  console.log(result);
-		});
-	}
+	// if (window.Notification && window.Notification.requestPermission) {
+	// 	window.Notification.requestPermission().then(function(result) {
+	// 	  console.log(result);
+	// 	});
+	// }
 }
 
 export function showNotification(title, message) {
