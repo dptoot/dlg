d5e117696fb7b442c0ccd7d4675ae50bfea5cf27
James Lewis <dptoot@gmail.com>
2016-12-11 11:40:13 -0500

    add web notifications

--- app/web/notifications.js
+++ app/web/notifications.js
@@ -1,18 +1,35 @@
+import NotifyLib from 'notifyjs';
+const Notify = NotifyLib.default;
 const src = require('./images/doug-icon.png');
 
+function onPermissionGranted() {
+    console.log('Permission has been granted by the user');
+}
+
+function onPermissionDenied() {
+    console.warn('Permission has been denied by the user');
+}
+
 export function requestNotificationPermission() {
-	// if (window.Notification && window.Notification.requestPermission) {
-	// 	window.Notification.requestPermission().then(function(result) {
-	// 	  console.log(result);
-	// 	});
-	// }
+	if (Notify.isSupported()) {
+    	Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+	}
 }
 
 export function showNotification(title, message) {
-	if (window.Notification) {
-		new Notification(title, {
+	
+	if (!Notify.needsPermission) {
+    	const myNotification = new Notify(title, {
 		  body: message,
 		  icon: src,
+		  timeout: 20000,
+		  closeOnClick: true, 
 		});
+
+		myNotification.show();
+
+	} else if (Notify.isSupported()) {
+	    requestNotificationPermission(onPermissionGranted, onPermissionDenied);
 	}
+	
 }
